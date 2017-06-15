493837a0cc4d527dfe4a61ad7baf520faeaa55df
Kevin-Moon <metamind@korea.ac.kr>
2017-06-16 00:46:34 +0900

    Add files via upload

---
+++ a6/Ridge_vs_SVM_vs_DT_vs_CNN.py
@@ -0,0 +1,123 @@
+# yup_Ridge_vs_SVM_vs_DT_vs_CNN.py
+
+from sklearn.datasets import fetch_mldata
+from sklearn.linear_model import RidgeClassifierCV
+import numpy as np
+import nnet
+from sklearn.linear_model import RidgeClassifierCV
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+import time
+
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.model_selection import KFold, cross_val_score
+
+
+# Fetch data - caution this is 55MB for the first download
+mnist = fetch_mldata('MNIST original', data_home='./data')
+
+# split the dataset into train and test and normalize
+# the first 60000 examples already are the training set
+split = 60000
+X_train = np.reshape(mnist.data[:split], (-1, 1, 28, 28))/255.0
+Y_train = np.array([int(x) for x in mnist.target[:split]])
+X_test = np.reshape(mnist.data[split:], (-1, 1, 28, 28))/255.0
+Y_test = mnist.target[split:]
+
+# for speed purpose do not train on all examples
+n_train_samples = 1000
+n_classes = 10
+
+# Optimize Filter
+# n_feat1, n_feat2 = optimize_filter(5000, n_classes, X_train, Y_train, split)
+
+# SETUP two-layers CONVnet
+nn = nnet.NeuralNetwork(
+    layers=[
+        nnet.Conv(
+            n_feats=8,
+            filter_shape=(5, 5),
+            strides=(1, 1),
+            weight_scale=0.1,
+        ),
+        nnet.Activation('relu'),
+        nnet.Pool(
+            pool_shape=(2, 2),
+            strides=(2, 2),
+            mode='max',
+        ),
+        nnet.Conv(
+            n_feats=16,
+            filter_shape=(5, 5),
+            strides=(1, 1),
+            weight_scale=0.1,
+        ),
+        nnet.Activation('relu'),
+        nnet.Flatten(),
+        nnet.Linear(
+            n_out=n_classes,
+            weight_scale=0.1,
+        ),
+        nnet.LogRegression(),
+    ],
+)
+
+RidgeClassifierCV_result = []
+LinearSVC_result = []
+DecisionTreeClassifier_result = []
+
+for i in range(20):
+
+    # Data Set
+    # this is very important here - we select random subset!!!
+    # this is done to ensure that the minibatches will actually see
+    # different numbers in each training minibatch!
+
+    train_idxs = np.random.randint(0, split-1, n_train_samples)
+
+    # CNN Data input
+    X_tr_CNN = X_train[train_idxs, ...]
+    Y_tr_CNN = Y_train[train_idxs, ...]
+
+    # Other Data input
+    X_tr = mnist.data[train_idxs, ...]/255.0
+    Y_tr = mnist.target[train_idxs]
+    X_t = mnist.data[split:, ...] / 255.0
+    Y_t = mnist.target[split:]
+
+    # Try Ridge Classifier
+    rcv = RidgeClassifierCV().fit(X_tr, Y_tr)
+    ridge_result = rcv.score(X_t, Y_t)
+    RidgeClassifierCV_result.append(ridge_result)
+    print('Ridge         [',i+1,']', ridge_result)
+
+    # Try linear svc
+    lsvc_clf = LinearSVC()
+    param_grid_lsvc = {'C': [0.1, 1, 10, 100, 1000]}
+    grid_search_lsvc = GridSearchCV(lsvc_clf, param_grid_lsvc)
+    lsvc = grid_search_lsvc.fit(X_tr, Y_tr)
+    linear_result = lsvc.score(X_t, Y_t)
+    LinearSVC_result.append(linear_result)
+    print('SVC           [',i+1,']', linear_result)
+
+    # Try Decision Tree classifier
+    dtree_clf = DecisionTreeClassifier()
+    param_grid_dtree = {'max_depth': [2, 3, 4, 5, 6, None]}
+    grid_search_dtree = GridSearchCV(dtree_clf, param_grid=param_grid_dtree)
+    dtc = grid_search_dtree.fit(X_tr, Y_tr)
+    dtc_result = dtc.score(X_t, Y_t)
+    DecisionTreeClassifier_result.append(dtc_result)
+    print('DecisionTree [',i+1,']', dtc_result)
+
+    # Try CNN
+    nn.fit(X_tr_CNN, Y_tr_CNN, learning_rate=0.1, max_iter=15, batch_size=64)
+
+
+print('******************')
+print('** Final Result **')
+print('******************')
+print('RidgeClassifierCV      :',RidgeClassifierCV_result,'Mean :', np.mean(np.array(RidgeClassifierCV_result))*100,'(%)', 'Std :', np.std(np.array(RidgeClassifierCV_result)))
+print('LinearSVC              :',LinearSVC_result, 'Mean :', np.mean(np.array(LinearSVC_result))*100,'(%)', 'Std :', np.std(np.array(LinearSVC_result)))
+print('DecisionTreeClassifier :',DecisionTreeClassifier, 'Mean :', np.mean(np.array(DecisionTreeClassifier_result))*100,'(%)', 'Std :', np.std(np.array(DecisionTreeClassifier_result)))
---
+++ a6/filter_optimizer.py
@@ -0,0 +1,140 @@
+# yup_Ridge_vs_SVM_vs_DT_vs_CNN.py
+
+from sklearn.datasets import fetch_mldata
+import numpy as np
+import nnet
+import time
+from sklearn.model_selection import KFold, cross_val_score
+
+# 10-fold cross validation
+k_fold = KFold(n_splits=10)
+
+def optimize_filter(n_train_samples,n_classes,X_train,Y_train, split):
+    train_idxs = np.random.randint(0, split - 1, n_train_samples)
+    n_feats = [2, 4, 6, 8, 12, 16]  # for the second layer!
+
+    X_train = X_train[train_idxs, ...]
+    Y_train = Y_train[train_idxs, ...]
+
+    result = []
+    for index, nf in enumerate(n_feats):
+        fold_result = []
+        print('*** Starting 1-layer test of feat [', n_feats[index], ']...')
+
+        # SETUP one-layer CONVnet
+        nn = nnet.NeuralNetwork(
+            layers=[
+                nnet.Conv(
+                    n_feats=nf,
+                    filter_shape=(5, 5),
+                    strides=(1, 1),
+                    weight_scale=0.1,
+                ),
+                nnet.Activation('relu'),
+                nnet.Flatten(),
+                nnet.Linear(
+                    n_out=n_classes,
+                    weight_scale=0.1,
+                ),
+                nnet.LogRegression(),
+            ],
+        )
+
+        # TRAINING
+        for train_indices, valid_indices in k_fold.split(np.array(X_train)):
+            np.random.shuffle(train_indices)
+            #print(train_indices, valid_indices)
+            X_tr = X_train[train_indices, ...]
+            Y_tr = Y_train[train_indices, ...]
+            X_val = X_train[valid_indices, ...]
+            Y_val = Y_train[valid_indices, ...]
+
+            # Train neural network
+            t0 = time.time()
+            nn.fit(X_tr, Y_tr, learning_rate=0.1, max_iter=15, batch_size=30)
+            t1 = time.time()
+
+            # Evaluate on test data
+            onelayer_result = nn.error(X_val, Y_val)
+            fold_result.append(onelayer_result)
+
+            print('Duration: %.1fs' % (t1 - t0))
+            print('Valid error rate: %.4f' % onelayer_result)
+
+        # save the result for each n_feat
+        result.append(np.mean(np.array(fold_result)))
+
+    best_one_layer = n_feats[result.index(max(result))]
+
+
+    # Try two-layer CONVnet
+
+    two_layer_result = []
+    for index, nf in enumerate(n_feats):
+        fold_result = []
+        print('*** Starting 2-layers-test of feat [', n_feats[index], ']...')
+
+        # SETUP two-layers CONVnet
+        nn = nnet.NeuralNetwork(
+            layers=[
+                nnet.Conv(
+                    n_feats=best_one_layer,
+                    filter_shape=(5, 5),
+                    strides=(1, 1),
+                    weight_scale=0.1,
+                ),
+                nnet.Activation('relu'),
+                nnet.Pool(
+                    pool_shape=(2, 2),
+                    strides=(2, 2),
+                    mode='max',
+                ),
+                nnet.Conv(
+                    n_feats=n_feats,
+                    filter_shape=(5, 5),
+                    strides=(1, 1),
+                    weight_scale=0.1,
+                ),
+                nnet.Activation('relu'),
+                nnet.Flatten(),
+                nnet.Linear(
+                    n_out=n_classes,
+                    weight_scale=0.1,
+                ),
+                nnet.LogRegression(),
+            ],
+        )
+
+        # TRAINING
+        for train_indices, valid_indices in k_fold.split(np.array(X_train)):
+            np.random.shuffle(train_indices)
+            #print(train_indices, valid_indices)
+            X_tr = X_train[train_indices, ...]
+            Y_tr = Y_train[train_indices, ...]
+            X_val = X_train[valid_indices, ...]
+            Y_val = Y_train[valid_indices, ...]
+
+            # Train neural network
+            t0 = time.time()
+            nn.fit(X_tr, Y_tr, learning_rate=0.1, max_iter=15, batch_size=30)
+            t1 = time.time()
+
+            # Evaluate on test data
+            two_result = nn.error(X_val, Y_val)
+            fold_result.append(two_result)
+
+            print('Duration: %.1fs' % (t1 - t0))
+            print('Valid error rate: %.4f' % two_result)
+
+        # save the result for each n_feat
+        two_layer_result.append(np.mean(np.array(fold_result)))
+
+    best_two_layer = n_feats[two_layer_result.index(max(two_layer_result))]
+
+
+    print('One-layer result :', result)
+    print('One-Layer Optimum N_feat Value :', best_one_layer)
+    print('Two-layer result :', two_layer_result)
+    print('Two-Layer Optimum N_feat Value :', best_one_layer, best_two_layer)
+
+    return best_one_layer, best_two_layer
